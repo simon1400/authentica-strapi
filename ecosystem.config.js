c1ae63afa1a53c1a42ddca887637edabce7aa046
Your Name <>
2022-05-01 12:50:24 +0200

    pm2 fix env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,14 @@ module.exports = {
   apps : [{
     name   : "Authentica strapi",
     script : "yarn start",
+    env_production: {
+      HOST:"0.0.0.0",
+      PORT:1340,
+      APP_KEYS:"VtJmJ/asmp11LACRtwYAGg==,wtNAi4J+JnUUPEudas+jiA==,y8YhqV0Z8TjzlVkc6xPeEw==,7HixGcHKpG1Vk/i97tVn/g==",
+      API_TOKEN_SALT:"IzQfHmVUqqBk1JqzvEwbog==",
+      ADMIN_JWT_SECRET:" 9QjY3n+cWnVEcSGSzo5SPw==",
+      JWT_SECRET:"juwtn0n2V2NeaGjfZpCANw==",
+    }
   }],
 
   deploy : {
@@ -11,7 +19,7 @@ module.exports = {
       ref  : 'origin/main',
       repo : 'git@github.com:simon1400/authentica-strapi.git',
       path : '/var/www/authentica-original/strapi',
-      'pre-deploy-local': 'yarn && yarn build',
+      // 'pre-deploy-local': 'yarn && yarn build',
       'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
     }
   }
