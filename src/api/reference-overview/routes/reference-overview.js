a4d81e4631d03ac0594760562d1dc124ee847c05
Your Name <>
2022-05-02 19:13:04 +0200

    adsfsdf

---
+++ src/api/reference-overview/routes/reference-overview.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * reference-overview router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::reference-overview.reference-overview');
