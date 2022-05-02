fd2ca8ac6be37e65267696d34fc158b4a901b2c4
Your Name <>
2022-05-02 16:30:50 +0200

    fix model

--- src/api/reference-overview/controllers/reference-overview.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  reference-overview controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::reference-overview.reference-overview');
--- src/api/reference-overview/services/reference-overview.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * reference-overview service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::reference-overview.reference-overview');
