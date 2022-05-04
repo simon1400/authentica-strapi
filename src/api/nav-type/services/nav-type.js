4b309cf09a9198eacb9fa227c9e783c20960d18f
simon1400 <pechunka11@gmail.com>
2022-05-04 11:30:58 +0200

    fixed names

--- src/api/nav-type/services/nav-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nav-type service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::nav-type.nav-type');
