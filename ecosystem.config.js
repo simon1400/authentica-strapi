7062724c51c637d744f867bb7e28b707cd4f4d87
Your Name <>
2024-07-25 17:38:48 +0200

    deploy change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host : ['89.221.216.23'],
       ref  : 'origin/main',
       repo : 'git@github.com:simon1400/authentica-strapi.git',
-      path : '/var/www/authentica-original/strapi',
+      path : '/home/dimi/app/authentica-original/strapi',
       // 'pre-deploy-local': 'git add . && git commit -m "pm2 deploy auto commit" && git push origin main',
       'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
     }
