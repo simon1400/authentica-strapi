c457dfbace53df2e47960922951772d5abb29999
Your Name <>
2024-07-25 18:40:54 +0200

    safg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "Authentica strapi",
-    script : "yarn start"
+    script : "npm start"
   }],
 
   deploy : {
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:simon1400/authentica-strapi.git',
       path : '/home/dimi/app/authentica-original/strapi',
       // 'pre-deploy-local': 'git add . && git commit -m "pm2 deploy auto commit" && git push origin main',
-      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     }
   }
 };
